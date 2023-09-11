085c26c7782148d6bbfd22bc3d5ac72ca96f4df0
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-09-12 07:20:47 +0900

    Add: users feature

---
+++ db/schema.rb
@@ -0,0 +1,9 @@
+ActiveRecord::Schema[7.0].define(version: 2023_09_11_132710) do
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
